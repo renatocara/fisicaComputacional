ae13ef72aec06edebae88e55247a940349a7a849
Renato <rcara@atech.com.br>
2019-11-28 15:33:34 -0300

    Versao melhorada pro Julho

--- trabalho4_ex1_graficos_e_estimativas.py
+++ trabalho4_ex1_graficos_e_estimativas.py
@@ -1,84 +1,45 @@
-
-__author__ = "Renat Cara - 9301579"
-__email__ = "renato.cara@usp.br"
-
-'''
-4300218-2019 - EP 4 a
-Faça um programa em Python que faça um gráfico das quantidades 
-tan√w²mEᵢ/2ℏ²  √(V-E)/E √E/(V-E) 
-Em função de E, para E variando de 0 a 30 eV. Do seu gráfico, faça estimativas
-visuais para os seis primeiros níveis de energia possíveis
-'''
-
-
-from math import tan, sqrt, pi, atan, sin, sinh, cos, cosh, nan
+from math import tan, sqrt, pi, nan
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+import matplotlib.style
 import numpy as np
 
-eV=1.6022e-19 #joules
-nm = 1.0e-9 #metros
-c=3e8
-m=9.1094e-31 #kg
-w= 1.0 * nm 
-V=20.0 * eV 
-E_max = 30 * eV 
-h=6.626e-34 #4.135667696e-15 #eV
-hb=h/(2.0*pi)
-
-#Solução paleativa de baixo custo alternativa para lidar com os pontos onde E>V e a curva fica complexa
-treshold_niveis =  3.5e-18 #Ponto a partir do qual não é necessário mais contar os niveis 
-def F(i):
-    '''
-    Define o conjunto de funções
-
-    Retorna a função adequada de acordo com o índice e não o valor dela
-
-    ⎰ f(x) =  √(V-E)/E   se i par
-    ⎱ f(x) = -√E/(V-E)    se i impar
-
-    '''
-    functions_set = {
-        0:lambda E:((V-E)/E)**(1.0/2.0),
-        1:lambda E:-((E/(V-E))**(1.0/2.0)),
-    }
-    return functions_set[i%2]
+mpl.style.use('seaborn-bright')
 
 
-def F_geral(Ei):
-    '''
-        tan√w²mEᵢ/2ℏ²
-    '''
-    return lambda E: complex(tangent((((w**2) * m * Ei(E))/(2*(hb**2)))**(1.0/2.0)))
 
 
-def F_e(i):
-    '''
-    ⎰ f(x) = tan√w²mEᵢ/2ℏ² - √(V-E)/E       se i par
-    ⎱ f(x) = tan√w²mEᵢ/2ℏ² + √E/(V-E)       se i impar
-    '''
-    return lambda E: F_geral(lambda x: x)(E) - F(i)(E)
+hb=4.135667696e-15/(2.0*pi)
+w= 1.0e-9 
+m=9.1094e-31/1.6022e-19
+E_max = 20 
+V=20.0 
+MAX=8.0
+def f_energia_par(E):
+    return ((V-E)/E)**(1/2)
 
+def f_energia_impar(E):
+    return -((E/(V-E))**(1/2))
 
-def tangent(c):
-    '''
-    Calcula a tangente de um número complexo se aproveitando da identidade
-    https://proofwiki.org/wiki/Tangent_of_Complex_Number 
-    '''
-    if isinstance(c, complex):
-        a = c.real
-        b = c.imag
-        res = (sin(a) *cosh(b) + 1j * cos(a) * sinh(b))/(cos(a) *cosh(b) - 1j * sin(a) * sinh(b))
-        return res
+def f_energia_par_impar(i, E):
+    if i%2==0:
+        return f_energia_par(E)
     else:
-        return tan(c)
+        return f_energia_impar(E)
 
 
 
-E = np.linspace(0, E_max, 300000)
+def fEnergia(Ei):
+    return tan(complex((((w**2) * m * Ei)/(2*(hb**2)))**(1.0/2.0)).real)
 
-f_par = F(0)
-f_impar = F(1)
-F_geral(lambda x: x)
+
+def F_e(i, E):
+    return fEnergia(E) - f_energia_par_impar(i,E)
+
+
+
+E = np.linspace(0, E_max, 1000)
+
 
 x = []
 y = []
@@ -86,68 +47,42 @@ par_x = []
 par_y = []
 impar_x = []
 impar_y = []
-roots_x = []
-roots_y = []
+
 
 for e in E:
     _x = float(e)
-    try:
-        _y = F_geral(lambda x: x)(_x)
+    if _x != V and _x != 0.0:
+        _y = fEnergia(_x)
 
-        _y_par = complex(f_par(_x)).real
-        _t_impar = complex(f_impar(_x)).real
+        _y_par = f_energia_par(_x)
+        _y_impar = f_energia_impar(_x)
         
         par_y.append(_y_par)
         par_x.append(_x)
         
-        impar_y.append(_t_impar)
+        impar_y.append(_y_impar)
         impar_x.append(_x)
         
-        #Remove a descontinuidade
-        if abs(_y) < 1000:
-            y.append(_y.real)
+        if abs(_y) < MAX:
+            y.append(_y)
         else:
             y.append(nan)
 
         x.append(_x)
-        epsilon = 0.1
-        
-        if _x > 3.7e-20 and _x < 0.1e-18:
-            epsilon = 0.1
-        else :
-            epsilon = 0.001
-
-        #Acha as raizes da eq
-        if _x < treshold_niveis and (abs(_y - _y_par) <= epsilon or abs(_y - _t_impar) <= epsilon) :
-            
-            if len(roots_y) < 1 or abs(_y - roots_y[-1]) > 0.1:
-                roots_y.append(_y.real)
-                roots_x.append(_x)
                 
 
-    except:
-        #Remove a descontinuidade
-        x.append(_x)
-        y.append(nan)
     
-plt.plot(par_x, par_y , label='√V-E/E')
-plt.plot(impar_x, impar_y , label='√E/V-E')
-plt.plot(x, y , label='tan√w²mEᵢ/2ℏ²')
-plt.ylim(bottom = -7.0, top=14.0)
-plt.scatter(roots_x, roots_y, label="Níveis de energia")
-plt.xlabel('Joules')
-plt.ylabel('Joules')
-plt.grid( linewidth=0.2)
-plt.title("Energias")
+plt.plot(par_x, par_y , label='Par')
+plt.plot(impar_x, impar_y , label='Impar')
+plt.plot(x, y , label='Tangente')
+plt.ylim(bottom = MAX*-1, top=MAX)
+plt.xlabel('eV')
+plt.ylabel('')
+plt.grid()
+
+plt.title('')
 
 plt.legend()
-
-print("*"*80)
-print("Níveis de energia")
-print("")
-print("{:3}\t{:5}".format("Nível", "Energia"))
-for n in range(len(roots_y)):
-    print("{}\t{:.2f}eV".format(format(n+1, '03'), roots_x[n]/eV))
-print("*"*80)
 plt.show()
+
 
--- trabalho4_ex2_primeiros_niveis_energia_busca_binaria.py
+++ trabalho4_ex2_primeiros_niveis_energia_busca_binaria.py
@@ -1,73 +1,40 @@
 
-'''
-4300218-2019 - EP 4 b)
-Escreva um segundo programa em Python que calcule os seis primeiros níveis de
-energia usando busca binária com uma precisão de 0,001 eV
-
-'''
 from math import tan, sqrt, pi, atan, sin, sinh, cos, cosh
 import matplotlib.pyplot as plt
 import numpy as np
 
-eV=1.6022e-19 #joules
-nm = 1.0e-9 #metros
-c=3e8
-m=9.1094e-31 #kg
-w= 1.0 * nm 
-V=20.0 * eV 
-E_max = 30 * eV 
-h=6.626e-34 #4.135667696e-15 #eV
-hb=h/(2.0*pi)
 
-#Solução paleativa de baixo custo alternativa para lidar com os pontos onde E>V e a curva fica complexa
-treshold_niveis =  3.5e-18 #Ponto a partir do qual não é necessário mais contar os niveis 
-def F(i):
-    '''
-    Define o conjunto de funções
+hb=4.135667696e-15/(2.0*pi)
+w= 1.0e-9 
+m=9.1094e-31/1.6022e-19
+E_max = 20 
+V=20.0 
+MAX=8.0
+def f_energia_par(E):
+    return ((V-E)/E)**(1/2)
 
-    Retorna a função adequada de acordo com o índice e não o valor dela
+def f_energia_impar(E):
+    return -((E/(V-E))**(1/2))
 
-    ⎰ f(x) =  √(V-E)/E   se i par
-    ⎱ f(x) = -√E/(V-E)    se i impar
-
-    '''
-    functions_set = {
-        0:lambda E:((V-E)/E)**(1.0/2.0),
-        1:lambda E:-((E/(V-E))**(1.0/2.0)),
-    }
-    return functions_set[i%2]
-
-
-def F_geral(Ei):
-    '''
-        tan√w²mEᵢ/2ℏ²
-    '''
-    return lambda E: complex(tangent((((w**2) * m * Ei(E))/(2*(hb**2)))**(1.0/2.0)))
-
-
-def F_e(i):
-    '''
-    ⎰ f(x) = tan√w²mEᵢ/2ℏ² - √(V-E)/E       se i par
-    ⎱ f(x) = tan√w²mEᵢ/2ℏ² + √E/(V-E)       se i impar
-    '''
-    return lambda E: F_geral(lambda x: x)(E) - F(i)(E)
-
-
-def tangent(c):
-    '''
-    Calcula a tangente de um número complexo se aproveitando da identidade
-    https://proofwiki.org/wiki/Tangent_of_Complex_Number 
-    '''
-    if isinstance(c, complex):
-        a = c.real
-        b = c.imag
-        res = (sin(a) *cosh(b) + 1j * cos(a) * sinh(b))/(cos(a) *cosh(b) - 1j * sin(a) * sinh(b))
-        return res
+def f_energia_par_impar(i, E):
+    if i%2==0:
+        return f_energia_par(E)
     else:
-        return tan(c)
+        return f_energia_impar(E)
 
 
-def busca_binaria(f,a,b,N):
+
+def fEnergia(Ei):
+    return tan(complex((((w**2) * m * Ei)/(2*(hb**2)))**(1.0/2.0)).real)
+
+
+def F_par(E):
+    return fEnergia(E) - f_energia_par(E)
+
+def F_impar(E):
+    return fEnergia(E) - f_energia_impar(E)
+
+def biseccao(f,a,b,N):
   
     if f(a)*f(b) >= 0:
         return None
@@ -90,65 +57,32 @@ def busca_binaria(f,a,b,N):
             return None
     return (a_n + b_n)/2
 
-#Pontos para busca binária obtidos através do 
-#gráfico gerado por trabalho4_ex1_graficos_e_estimativas.py
-raizes_poco_1_nm = [
-5.1e-21
-,1.21e-19
-,3.0e-19
-,5.8e-19
-,9.8e-19
-,1.4e-18
-,2.0e-18
-,2.8e-18
-,3.3e-18
-,4.0e-18,
-]
 
-raizes_poco_meio_nm = [
-9.0e-20
-,4.3e-19
-,1.18e-18
-,2.18e-18
-,3.8e-18
-]
-
-raizes_poco_V_40 = [
-2.6e-20
-,1.4e-19
-,3.4e-19
-,6.0e-19
-,1.13e-18
-,1.5e-18
-,2.3e-18
-,2.9e-18
-,3.8e-18
-
-]
-
-
-
-raizes = raizes_poco_V_40
+limites = [
+    0.04,
+    0.7,
+    2.0,
+    3.8,
+    6.7,
+    9.33,
+    13.0,
+    17.0
+    ]
 a = 0.0
-b = raizes[0]
+b = limites[0]
 n = 1
-print(80*"-")
-print("Usando busca binária achamos as seguintes raízes")
-print(80*"-")
-print("N\tEnergia (J)\t\t\tEnergia (eV)")
-for i in range(len(raizes) - 2):
-    a = raizes[i]
+
+print("Níveis de Energia")
+for i in range(len(limites) - 1):
+    a = limites[i]
     
-    b = (raizes[i+1])
-    r = busca_binaria(lambda x: F_e(0)(x).real,a,b, 10000)
+    b = (limites[i+1])
+
+    r = biseccao(F_par,a,b, 10000)
     if r is None:
-        r = busca_binaria(lambda x: F_e(1)(x).real,a,b, 10000)
+        r = biseccao(F_impar,a,b, 10000)
     
     if r is not None:
-        
-        print("n= {}\t{}\t\t{:.2f}eV ".format(n, r, r/eV))
+        print(n, r)
         n = n+1
-    #print(raizes_originais[i], "{:.3}".format(raizes_originais[i]/eV),  r, "{:.3}".format(r/eV))
-    a = raizes[i]
-    b = raizes[i+1]
 
